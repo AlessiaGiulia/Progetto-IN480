508c839f46d743334124aa42f8d75669a82f29f0
AdDesi <desireeadiutori@hotmail.it>
2017-12-05 21:15:11 +0100

    embedTraversal

--- larStruct.jl
+++ larStruct.jl
@@ -1,69 +1,65 @@
 using PyCall
 @pyimport scipy
-#chissà che succede
+
 
 function prepKey (args)
-
-            return  
-
+	return  
 end
 
 function fixedPrec(PRECISION)
-
-            function fixedPrec0(value)
-
-                        out=round(value,PRECISION)
-
-                        if out==-0.0
-
-                                   out=0.0
-
-                        end
-
-                        return string(out)
-
-            end
-
-            return fixedPrec0
-
+	function fixedPrec0(value)
+		out=round(value,PRECISION)
+		if out==-0.0
+			out=0.0
+		end
+		return string(out)
+	end
+	return fixedPrec0
 end
 
  
 
 function vcode(PRECISION=4)
-  function vcode0(vect)
-   return prepKey(map(fixedPrec(PRECISION),vect))
-  end
-  return vcode0
+	function vcode0(vect)
+		return prepKey(map(fixedPrec(PRECISION),vect))
+	end
+	return vcode0
 end
 
 
 function t(args…)
-    d=length(args)
-    mat=eye(d+1)
-
-    for k in range(1,d)
-             mat[k,d+1]=args[k]
-    end
-
-    return mat
+	d=length(args)
+	mat=eye(d+1)
+	for k in range(1,d)
+        	mat[k,d+1]=args[k]
+	end
+	return mat #vedere se bisogna farla di tipo Mat
 end
 
  
 
 function s(args…)
-   d=length(args)
-   mat=eye(d+1)
-   for k in range(1,d)
-      mat[k,k]=args[k]
-   end
-   return mat
+	d=length(args)
+	mat=eye(d+1)
+	for k in range(1,d)
+		mat[k,k]=args[k]
+	end
+	return mat
 end
 
 
 
 function removeDups(CW)
-	CW=[(map(tuple,CW))]
+	CW=collect(tuple(CW...))
+	CWs=collect(tuple(sort(CW,rev=true)...))
+	no_duplicates=Dict()
+	for f in CWs
+		no_duplicates[f] = []
+	end
+	for f in CW
+	no_duplicates[tuple(sort(collect(f))...)]=[f]
+	end
+	CW=[f[1] for f in values(no_duplicates)]
 	return CW
 end 
 
@@ -98,118 +94,78 @@ function larBoundary(self)
 		V,FV,EV=data
 		return V,FV,EV
 	else
-	return "<Struct name $self.__name__() : boundary non computable"   #controllare che effettivamente ritorna il nome della struct
+		return "<Struct name $self.__name__() : boundary non computable"   #controllare che effettivamente ritorna il nome della struct
 	end
 end
 
 
 
- 
+
 
 function struct2lar(structure,metric=ID)
-
-            listOfModels=evalStruct(structure)
-
-            vertDict= Dict()
-
-            index,defaultValue,CW,W,FW = 0,-1,[],[],[]
-
-            for model in listOfModels
-
-                        if  length(model)==2
-
-                                   V,FV=model
-
-                        elseif
-
-                                   V,FV,EV=model
-
-                        end
-
-                        for (k,incell) in enumerate(FV)
-
-                                   outcell=[]
-
-                                   for v in incell
-
-                                   	key=vcode(4)(V[v])
-
-                                   	if get(vertDict,key,defaultValue)==defaultValue
-
-                                        	indexx =index+1
-
-                                                vertDict[key]=index
-
-                                                outcell =append!(outcell,index])
-						W= append!(W,eval(key))                   
-                                           else
-
-                                                 outcell= append!(outcell,vertDict[key])
-
-                                               end
-
-                                   CW =append!(CW,[outcell])
-
-                                   End
-
-                        end
-
-                        If length(model)==3
-
-                                   outcell=[]
-
-                                   for v in incell
-
-                                               key=vcode(4)(V[v])
-
-                                               if get(vertDict,key,defaultValue)==defaultValue
-
-                                                           index =index+1
-
-                                                           vertDict[key]=index
-
-                                                           outcell =append!(outcell,[index])
-
-                                                           W= append!(W,eval(key))                   
-
-                                               else
-
-                                                           outcell= append!(outcell,vertDict[key])
-
-                                               end
-
-                                               FW =append!(FW,[outcell])
-
-                                   end
-
-                        end
-
-                        if length(model)==2
-
-                                   elseif length(CW[0])==2
-
-                                               CW= list(set(AA(tuple)(AA(sorted)(CW))))
-
-                                    else
-
-                                               CW= removeDups(CW)
-
-                                   return metric(W),CW
-
+	listOfModels=evalStruct(structure)
+	vertDict= Dict()
+	index,defaultValue,CW,W,FW = 0,-1,[],[],[]
+	for model in listOfModels
+		if  length(model)==2
+			V,FV=model
+		elseif lenght(model)==3
+			V,FV,EV=model
+		end
+		for (k,incell) in enumerate(FV)
+			outcell=[]
+			for v in incell
+				key=vcode(4)(V[v])
+				if get(vertDict,key,defaultValue)==defaultValue
+					index =index+1
+                                        vertDict[key]=index
+					outcell =append!(outcell,index])
+					W= append!(W,eval(key))                   
+				else
+					outcell= append!(outcell,vertDict[key])
+				end
+			end
+			CW =append!(CW,[outcell])
+		end
+		if length(model)==3
+			for (k,incell) in enumerate(FV)
+				outcell=[]
+				for v in incell
+					key=vcode(4)(V[v])
+					if get(vertDict,key,defaultValue)==defaultValue
+						index =index+1
+						vertDict[key]=index
+						outcell =append!(outcell,[index])
+						W=append!(W,eval(key))                   
+					else
+						outcell= append!(outcell,vertDict[key])
+					end
+				end
+				FW =append!(FW,[outcell])
+			end
+		end
+	end
+	if length(model)==2
+		elseif length(CW[0])==2
+			CW=list(set(AA(tuple)(AA(sorted)(CW))))#da cambiare
+		else
+			CW=removeDups(CW)
+		return metric(W),CW
+	end
+	if length(model)==3
+		FW=list(set(AA(tuple)(AA(sorted)(FW))))#da cambiare
+		CW=removeDups(CW)
+		return metric(W),CW,FW
+	end
 end
 
-if length(model)==3
-
-            FW= list(set(AA(tuple)(AA(sorted)(FW))))
-
-                                    CW= removeDups(CW)
-
-                                   Return metric(W),CW,FW
-
-                        end
-
-            end
-
+function embedTraversal(cloned,obj,n,suffix)
+	for i in range(1,length(obj))
+		if (isa(obj[i],tuple) ||isa(obj[i],Array))&& length(obj[i])==2 #potrebbe dare problemi in ndarray
+			V,EV=obj[i]
+			V=[v+
+		end
+	end
 end
 
  
