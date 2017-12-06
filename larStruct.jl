3d63a069af0a886a2a1ce1e74a7f18ee5258440c
AdDesi <desireeadiutori@hotmail.it>
2017-12-06 18:11:33 +0100

    larEmbed

--- larStruct.jl
+++ larStruct.jl
@@ -33,7 +33,7 @@ function t(args…)
 	for k in range(1,d)
         	mat[k,d+1]=args[k]
 	end
-	return mat #vedere se bisogna farla di tipo Mat
+	return mat.view(Mat) #vedere view dopo aver fatto la classe Mat
 end
 
  
@@ -47,6 +47,22 @@ function s(args…)
 	return mat
 end
 
+
+function larEmbed(k)
+	function larEmbed0(model)
+		if k>0
+			model[1]=[append!(v,fill(0.0,k)) for v in model[1]]
+		elseif k<0
+			for n in range(1,length(model[1][1])+k)	#vedere se esiste una funzione più pratica
+				for v in model[1]
+					pop!(v)
+				end
+			end
+		end3
+		return model
+	end
+	return larEmbed0
+end
 
 
 function removeDups(CW)
@@ -163,8 +179,56 @@ function embedTraversal(cloned,obj,n,suffix)
 	for i in range(1,length(obj))
 		if (isa(obj[i],tuple) ||isa(obj[i],Array))&& length(obj[i])==2 #potrebbe dare problemi in ndarray
 			V,EV=obj[i]
-			V=[v+
+			#V=[append!(v,fill(0.0,n)) for v in V]    #provare a vedere se girano entrambe allo stesso modo
+			dimadd=fill([0.0],n)	#controllare se esiste una funzione
+			for k in dimadd
+				for v in V
+					append!(v,k)
+				end
+			end
+			append!(cloned.body,(V,EV))	#controllare dopo aver fatto la classe Struct
+		elseif (isa(obj[i],tuple) ||isa(obj[i],Array))&& length(obj[i])==3 #potrebbe dare problemi in ndarray
+			V,FV,EV=obj[i]
+			dimadd=fill([0.0],n)	#controllare se esiste una funzione
+			for k in dimadd
+				for v in V
+					append!(v,k)
+				end
+			end
+			append!(cloned.body,(V,FV,EV))	#controllare dopo aver fatto la classe Struct
+		elseif isa(obj[i],Mat)
+			mat=obj[i]
+			d,d=size(mat)
+			newMat=eye(d+n*1)
+			for h in range(1,d-1)
+				for k in range(1,d-1)
+					newMat[h,k]=mat[h,k]
+				end
+				newMat[h,d-1+n*1]=mat[h,d-1]
+			end
+			append!(cloned.body,newMat.view(Mat))#controllare view 
+		elseif isa(obj[i],Struct)	#controllare dopo aver fatto la classe Struct
+			newObj=Struct()
+			newObj.box=hstack((obj[i].box,[fill([0],n),fill([0],n)]))	#vedere a chi corrisponde hstack
+			newObj.category=obj[i].category
+			append!(cloned.body,embedTraversal(newObj,obj[i],n,suffix))	#controllare dopo aver fatto la classe Struct
 		end
+	end
+	return cloned
+end
+
+function embedStruct(n)
+	function embedStruct0(struct,suffix="New")
+		if n==0
+			return struct, length(struct.box[1])
+		end
+		cloned=Struct()
+		cloned.box=hstack((struct.box,[fill([0],n),fill([0],n)]))	#vedere a chi corrisponde hstack
+		cloned.name=string(object_id(cloned))
+		cloned.category=struct.category
+		cloned.dim=struct.dim+n
+		cloned=embedTraversal(cloned,struct,n,suffix)
+		return cloned
 	end
 end
 
@@ -188,7 +252,7 @@ function larApply (affineMatrix)
   return larApply0
 
   if len(model)==2
-    return [v[:-1] for v in V],CV
+    return [v[:-1] for v in V],CV		#una cosa simile la avevo anche io...ho fatto un for(in larEmbed),ricordiamoci di confrontare!
 
   elseif len(model)==3
     return [v[:-1] for v in V],CV,FV
@@ -196,4 +260,4 @@ function larApply (affineMatrix)
 end 
 
 
-
+	
